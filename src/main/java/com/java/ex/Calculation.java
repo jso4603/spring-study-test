825e684ad5b19607cdfee3d2aabd0db32923a818
장상옥 <jso46034@naver.com>
2021-04-10 00:29:59 +0900

    1. BeanStudy

--- src/main/java/com/java/ex/Calculation.java
+++ src/main/java/com/java/ex/Calculation.java
@@ -1,5 +1,52 @@
 package com.java.ex;
 
 public class Calculation {
-	
+
+	private int firstNum;
+	private int secondNum;
+
+	public int getFirstNum() {
+		return firstNum;
+	}
+
+	public void setFirstNum(int firstNum) {
+		this.firstNum = firstNum;
+	}
+
+	public int getSecondNum() {
+		return secondNum;
+	}
+
+	public void setSecondNum(int secondNum) {
+		this.secondNum = secondNum;
+	}
+
+	public void add() {
+		System.out.println("더하기");
+		int result = firstNum + secondNum;
+		System.out.println(firstNum + " + " + secondNum + " = " + result);
+		System.out.println("");
+	}
+
+	public void sub() {
+		System.out.println("빼기");
+		int result = firstNum - secondNum;
+		System.out.println(firstNum + " - " + secondNum + " = " + result);
+		System.out.println("");
+	}
+
+	public void mul() {
+		System.out.println("곱하기");
+		int result = firstNum * secondNum;
+		System.out.println(firstNum + " * " + secondNum + " = " + result);
+		System.out.println("");
+	}
+
+	public void div() {
+		System.out.println("나누기");
+		int result = firstNum / secondNum;
+		System.out.println(firstNum + " / " + secondNum + " = " + result);
+		System.out.println("");
+	}
+
 }
--- src/main/java/com/java/ex/MainClass.java
+++ src/main/java/com/java/ex/MainClass.java
@@ -5,6 +5,19 @@ public class MainClass {
 	public static void main(String[] args) {
 		// Test
 		System.out.println("Hello");
+
+		Calculation calculation = new Calculation();
+		calculation.setFirstNum(10);
+		calculation.setSecondNum(5);
+
+
+		calculation.add();
+		calculation.sub();
+		calculation.mul();
+		calculation.div();
+		calculation.add();
+
+		Calculation calculation2 = new Calculation();
 	}
 
 }
